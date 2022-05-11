b48d04b83b0338d3926a0f2215745cc533c96d9c
chun50405 <chun50405@gmail.com>
2022-05-11 10:42:58 +0800

    新增拍照功能

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry, delay } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+  ApiUrl: string = 'https://localhost:3100/';
+  userList:any
+  sexList:any
+  constructor(private http: HttpClient) {}
 
-  constructor() {}
 
+
+   getUser() {
+    return this.http.get(this.ApiUrl + 'medicalPersonnel')
+    // .pipe(delay(3000))
+    .toPromise()
+    .then((response) => {
+
+      this.userList = response
+      for(let theUser of this.userList) {
+        theUser.account = theUser.user ? theUser.user.account : ''
+
+      }
+      console.log('response=', response)
+      console.log('  this.userList=',   this.userList)
+
+    })
+  }
+
+   getSex() {
+    return this.http.get(this.ApiUrl + 'sex')
+    // .pipe(delay(2000))
+    .toPromise()
+    .then((response) => {
+      this.sexList = response
+      console.log('response=', response)
+      console.log('this.sexList=', this.sexList)
+    })
+  }
+
+  async ngOnInit() {
+    // await this.getUser()
+    // await this.getSex();
+   }
+
+  async ionViewWillEnter() {
+     await this.getUser()
+     await this.getSex();
+   }
+
+  async doRefresh(event) {
+     await this.getUser()
+     await this.getSex();
+   }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry, delay } from 'rxjs/operators';
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import { HTTP } from '@ionic-native/http/ngx';
+
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
-  constructor() {}
+  constructor(private http: HttpClient, private httpIonic: HTTP) {}
 
+
+  async takePicture() {
+    const image = await Camera.getPhoto({
+      quality: 100,
+      allowEditing: true,
+      saveToGallery: true,
+      resultType: CameraResultType.Base64,
+      promptLabelHeader: '照片',
+      promptLabelCancel: '取消',
+      promptLabelPhoto: '從相簿中',
+      promptLabelPicture: '拍張照片吧'
+    })
+    console.log('image=', image)
+
+    let uploadUrl = 'https://192.168.10.44:5679/image/upload';
+
+
+
+
+    // let postData = new FormData();
+    // postData.append('file', image.base64String)
+    // postData.append('type', image.format)
+
+
+    // let uploadResult =  await this.http.post(uploadUrl, postData).toPromise()
+    this.httpIonic.setServerTrustMode('nocheck')
+    // let uploadResult = await this.httpIonic.post(uploadUrl, postData, {})
+
+    // console.log('uploadResult=', uploadResult)
+    let postData = {
+      file: image.base64String,
+      type: image.format
+    }
+    this.httpIonic.post(uploadUrl, postData, {})
+    .then(function(data) {
+      console.log('data=', data)
+    })
+    .catch(function(err) {
+      console.log('err=', err)
+    })
+  }
+
+
+  ngOnInit() {
+
+  }
 }
